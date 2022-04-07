3ec19f0b72f0f4bf9111512cd4960dc58373bf05
ABDUR RAHMAN <abdurrahman.shaik@phenompeople.com>
2022-04-07 17:35:21 +0530

    MyFirstChallenge

--- hello.py
+++ hello.py
@@ -1,2 +1,13 @@
-print('Hello, world!')
-
+l=input().split()
+k=int(input())
+x=0
+for I in l:
+    i=int(I)
+    for J in l:
+        j=int(J)
+        if (i!=j) and ((i+j)==k):
+            print("true")
+            x=1
+            break
+    if x==1:
+        break        
